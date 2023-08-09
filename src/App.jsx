a598fb4fe4c093460f599babecf7ea6d7f041b5f
Gega <gega.abzianidze.1@btu.edu.ge>
2023-08-09 22:32:03 +0400

    Add Warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Controls from "./Pages/Mobile/Controls";
 import Connection from "./Pages/Desktop/Connection";
@@ -13,24 +13,39 @@ class App extends Component {
       ),
       controlDirection: null,
     };
+
+    this.modalRef = React.createRef(); // Create a ref to hold the modal element
+  }
+
+  componentDidMount() {
+    // Check if the ref is available and if the dialog is not already open
+    if (this.modalRef.current && !this.modalRef.current.open) {
+      this.modalRef.current.showModal();
+    }
   }
 
   render() {
     const { isMobile } = this.state;
 
-    if (!isMobile) {
-      return (
-        <div className="h-screen">
-          <Connection />
-        </div>
-      );
-    } else {
-      return (
-        <div className="h-screen">
-          <Controls />
-        </div>
-      );
-    }
+    return (
+      <div className="h-screen">
+        <dialog id="my_modal_3" className="modal" ref={this.modalRef}>
+          <form method="dialog" className="modal-box">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+            <h3 className="font-bold text-lg">Hello!</h3>
+            <p className="py-4">
+              Live website is not optimized and have some bugs and delay. Please
+              clone the repo and run it locally.
+              <br /> but you can still try it here. :)
+            </p>
+          </form>
+        </dialog>
+        {!isMobile ? <Connection /> : <Controls />}
+      </div>
+    );
   }
 }
+
 export default App;
